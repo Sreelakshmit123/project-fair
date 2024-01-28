3bcd10ec7779b3dd7d519c3cc985a09209ef3eeb
Sreelakshmit123 <tsreelakshmi88@gmail.com>
2024-01-28 19:57:55 +0530

    update

--- src/Pages/Auth.jsx
+++ src/Pages/Auth.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
-import {  Link } from 'react-router-dom'
+import React, { useState } from 'react'
+import {  Link, useNavigate } from 'react-router-dom'
 import Form from 'react-bootstrap/Form';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { registerAPI } from '../Services/allAPIs';
 
 
 function Auth({insideRegister}) {
+  const navigate = useNavigate()
+  const [userData,setUserData] = useState({
+    username:"",email:"",password:""
+  })
+  const handleRegister =async (e)=>{
+    e.preventDefault()
+    console.log(userData);
+    const {username,email,password} = userData
+    if(!username  ||  !email || !password){
+      toast.info("Please fill the form completly!!")
+    }else{
+      // toast.success("Proceed to api call")
+      try{
+          const result = await registerAPI(userData)
+          console.log(result);
+          if(result.status===200){
+            toast.success(`${result.data.username} has registered successfully!!!`)
+            setUserData({ username:"",email:"",password:""})
+            setTimeout(()=>{
+                navigate('/login')
+            },3000);
+          }else{
+            toast.warning(result.response.data)
+          }
+      }catch{
+        console.log(err);
+      }
+    }
+
+  }
   return (
     <div style={{width:'100%', height:'100vh'}} className='d-flex justify-content-center align-items-center'>
       <div className="container w-75">
@@ -22,21 +56,21 @@ function Auth({insideRegister}) {
                 <Form className="w-100">
 {insideRegister &&(
         <Form.Group className="mb-3" controlId="formBasicName">
-        <Form.Control type="text" placeholder="Enter Username" />
+        <Form.Control type="text" placeholder="Enter Username" onChange={e=>setUserData({...userData,username:e.target.value})} value={userData.username} />
       </Form.Group>)}
 
       <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={e=>setUserData({...userData,email:e.target.value})} value={userData.email}  />
       </Form.Group>
 
       <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" onChange={e=>setUserData({...userData,password:e.target.value})} value={userData.password}  />
       </Form.Group>
    
     {
       insideRegister?
       <div>
-        <button className='btn btn-light mb-2'>Register</button>
+        <button onClick={handleRegister} className='btn btn-light mb-2'>Register</button>
         <p>Already have an Account? click here to <Link to={'/login'}>Login</Link></p>
       </div>:
       <div>
@@ -50,6 +84,8 @@ function Auth({insideRegister}) {
           </div>
         </div>
       </div>
+
+      <ToastContainer autoClose={3000} theme='colored' />
     </div>
   )
 }
